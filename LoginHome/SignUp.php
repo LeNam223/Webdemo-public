0464120e1828939ce0deddf399b41f0283da18a3
Nam <namgiangtu2013@gmail.com.vn>
2021-07-21 09:50:41 +0900

    adding my files to my repository

--- LoginHome/SignUp.php
+++ LoginHome/SignUp.php
@@ -12,7 +12,6 @@ session_start();
   <!-- <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script> -->
   <!-- <script src="../JavaScript/login.js"></script> -->
 </head>
-
 <body>
   <h3>
   <?php
@@ -28,10 +27,10 @@ session_start();
         <h2 class="form-title" id="signup">Sign up</h2>
         <div class="form-holder">
           <input type="text" class="input" placeholder="Name" name="username">
-          <!-- <div class="password_container"> -->
+          <div class="password_container">
             <input type="password" class="input" placeholder="Password" name="password" id="myInput">
-            <!-- <button class="btn-show-pass" type="button" onclick="myFunction()"><p>show</p></button> -->
-          <!-- </div> -->
+            <button class="btn-show-pass" type="button" onclick="myFunction()"><p>show</p></button>
+          </div>
           <input type="email" class="input" placeholder="Email" name="email" />
         </div>
         <button type="submit" class="submit-btn">Sign up</button>
